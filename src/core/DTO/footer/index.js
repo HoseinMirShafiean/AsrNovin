f078d8f8a7952d7daeeada2418fe84027ab98461
Hosein Mirshafian <shm1386fm@gmail.com>
2024-11-02 22:47:13 +0330

    file v2

---
+++ src/core/DTO/footer/index.js
@@ -0,0 +1,11 @@
+ export const footer = {
+    cards:{
+        one: {title: "عصر نوین" , route: "/"},
+        two: {title: "تماس با عصر نوین" , route: ""},
+        three: {title: "محصولات" , route: "/products"},
+        four: {title: "مقالات" , route: "/articles"},
+        five: {title: "مدارک فنی" , route: "/download"},
+        six: {title: "درباره ما" , route: "/about"},
+        seven: {title: "خدمات پس از فروش" , route: "/afterSaleService"},
+    }
+}
--- src/core/footer/footer.jsx
+++ src/core/footer/footer.jsx
@@ -1,58 +1,77 @@
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TelegramIcon from '@mui/icons-material/Telegram';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
-import { Box, Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Box, Grid, Table, TableContainer } from "@mui/material";
+import { useNavigate } from 'react-router-dom';
 import siteLogo from '../../image/siteLogo.jpg';
+import { footer } from '../DTO/footer';
 
 
-function CreateData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-}
+const dot = [
+    {
+        id: 0,
+        title: footer.cards.one.title,
+        route: footer.cards.one.route
+    },
+    {
+        id: 1,
+        title: footer.cards.two.title,
+        route: footer.cards.two.route
+    },
+    {
+        id: 2,
+        title: footer.cards.three.title,
+        route: footer.cards.three.route
+    },
+    {
+        id: 3,
+        title: footer.cards.four.title,
+        route: footer.cards.four.route
+    },
+    {
+        id: 4,
+        title: footer.cards.five.title,
+        route: footer.cards.five.route
+    },
+    {
+        id: 5,
+        title: footer.cards.six.title,
+        route: footer.cards.six.route
+    },
+    {
+        id: 6,
+        title: footer.cards.seven.title,
+        route: footer.cards.seven.route
+    },
+]
 
-const rows = [
-    CreateData('Steuerungsbau GmbH', "Contact", "تابلوی فرمان آسانسور های هیدرولیک", "آیا سقوط آسانسور امکان پذیر است؟"),
-    CreateData('82166 Gräfelfing', "Technical support", "تابلوی فرمان آسانسور های کششی", "استانداردهای پله برقی"),
-    CreateData('Phone: +49 89 - 898 66 - 0', "Locations", "موتور آسانسور"),
-    CreateData('', "", "لوازم جانبی آسانسور",),
-    CreateData('', "", "پکیج کامل آسانسور"),
-    CreateData('', "", "برق اضطراری آسانسور",),
-];
+function Footer() {
+    const navigate = useNavigate();
+    const handelOnClick = (route) => {
+        navigate(route)
+    };
 
-
-
-function Footer(name, calories, fat, carbs, protein) {
     return (
         <Box className="footerBox">
             <TableContainer className="tableInFooter">
                 <Table aria-label="simple table">
-                    <TableHead>
-                        <TableRow className="tableRowInFooter">
-                            <TableCell align="right">عصر نوین</TableCell>
-                            <TableCell align="right">تماس با عصرنوین&nbsp;</TableCell>
-                            <TableCell align="right">محصولات&nbsp;</TableCell>
-                            <TableCell align="right">مقالات&nbsp;</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map((row) => (
-                            <TableRow
-                                key={row.name}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <TableCell className='lineHeight' component="th" scope="row" align="right">{row.name}</TableCell>
-                                <TableCell className='lineHeight' align="right">{row.calories}</TableCell>
-                                <TableCell className='lineHeight' align="right">{row.fat}</TableCell>
-                                <TableCell className='lineHeight' align="right">{row.carbs}</TableCell>
-                                <TableCell className='lineHeight' align="right">{row.protein}</TableCell>
-                            </TableRow>
-                        ))}
-                        <h5 className="socialInFooter">
-                            Find us on Social Media
-                        </h5>
-                        <InstagramIcon className="socialIconInFooter" />
-                        <TelegramIcon className="socialIconInFooter" />
-                        <WhatsAppIcon className="socialIconInFooter" />
-                    </TableBody>
+                    {dot.map((item, index) => (
+                        // <Grid container className='rowFooterBox'>
+                        <h6
+                            className='rowFooter'
+                            onClick={() => {
+                                handelOnClick(item.route)
+                            }}>
+                            {item.title}
+                        </h6>
+                        // </Grid>
+                    ))}
+                    <h5 className="socialInFooter">
+                        Find us on Social Media
+                    </h5>
+                    <InstagramIcon className="socialIconInFooter" />
+                    <TelegramIcon className="socialIconInFooter" />
+                    <WhatsAppIcon className="socialIconInFooter" />
                 </Table>
             </TableContainer>
             <Grid container className="bottomFooterBox">
